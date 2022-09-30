2b6cba928d2fee2d77a97030fae638e2c645b033
kasthuriSiva <113717864+kasthuriSiva@users.noreply.github.com>
2022-09-30 13:49:36 -0500

    Labcommit2.0

---
+++ U3_Boolean_If_Statements/ChevyLab/ChevyDriver.java
@@ -0,0 +1,11 @@
+package U3_Boolean_If_Statements.ChevyLab;
+
+import U3_Boolean_If_Statements.Pizza;
+
+public class ChevyDriver {
+    public static void main(String[] args) {
+        Chevy c1 = new Chevy (1990,15000,16.5,37850,48920,54888.24,"Silverado","Blue");
+
+        System.out.println(c1);
+    }
+}
