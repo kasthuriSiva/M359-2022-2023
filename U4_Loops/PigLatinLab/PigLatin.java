9703914a75bfb91771346c3798017a8bd7ca6f40
kasthuriSiva <113717864+kasthuriSiva@users.noreply.github.com>
2022-10-19 13:50:26 -0500

    PigLatin Part2

--- U4_Loops/PigLatinLab/PigLatin.java
+++ U4_Loops/PigLatinLab/PigLatin.java
@@ -3,7 +3,10 @@ package U4_Loops.PigLatinLab;
 public class PigLatin {
 
    public static String translateWordToPigLatin(String str){
+       return toPigLatin(str);
+   }
 
+   public static String convertOneWord(String str){
        String strNew = "";
        if(str.substring(0,1).equalsIgnoreCase("a")||str.substring(0,1).equalsIgnoreCase("e")||
                str.substring(0,1).equalsIgnoreCase("i")||str.substring(0,1).equalsIgnoreCase("o")||str.substring(0,1).equalsIgnoreCase("u")){
@@ -22,9 +25,14 @@ public class PigLatin {
        }
    }
 
-   /*public static String toPigLatin(){
+   public static String toPigLatin(String phrase){
+        String output ="";
+       for(int i=0; i<phrase.length(); i++){
+            if(phrase.charAt(i)==' '){
+                output += convertOneWord(phrase) + " ";
+            }
+       }
+        return output;
+    }
 
-   }
-
-    */
 }
