35cb18b2260cdc20518d666471c0fd29d4a6d5e5
kasthuriSiva <113717864+kasthuriSiva@users.noreply.github.com>
2022-10-21 13:48:38 -0500

    PigLatinDay4

--- U4_Loops/PigLatinLab/PigLatin.java
+++ U4_Loops/PigLatinLab/PigLatin.java
@@ -2,52 +2,71 @@ package U4_Loops.PigLatinLab;
 
 public class PigLatin {
 
-    public static String toPigLatin(String phrase){
-        findWord(phrase);
-        return phrase;
-    }
+    public static String toPigLatin(String phrase) {
+        boolean isDone = false;
+        int loc = -1;
+        String output = "";
 
-    public static String findWord(String phrase){
-       boolean isDone = false;
-       int loc = -1;
-       String output = "";
+        while (phrase.indexOf(" ") != -1) {
+            if(isDone == false) {
+                loc = phrase.indexOf(" ");
 
-        while (isDone==false){
-            loc = phrase.indexOf(" ");
+                if (loc != -1) {
+                    //if we find space join the part before " " with the part after " "
+                    String word = findWord(phrase);
+                    System.out.println(word);
+                    output += translateWordToPigLatin(word);
+                    phrase = phrase.substring(loc+1);
 
-            if(loc !=-1){
-                //if we find space join the part before " " with the part after " "
-                output+= phrase.substring(0,loc);
-                temp = translateWordToPigLatin(output);
+                } else {
+                    //Means no more " " found
+                    isDone = true;
+                }
 
-            }
-            else{
-                //Means no more " " found
-                isDone = true;
             }
 
         }
         return output;
     }
 
+    public static String findWord(String phrase){
+        System.out.println(phrase);
+       String temp = "";
+
+       boolean isSpace = false;
+       while(isSpace = false){
+           int i =0;
+           if(phrase.charAt(i) != ' '){
+               temp += phrase.charAt(i);
+           }
+           else{
+               isSpace = true;
+           }
+           i++;
+       }
+       return temp;
+    }
     public static String translateWordToPigLatin(String str){
         String strNew = "";
+        String vowels = "a e i o u";
 
-        if(str.substring(0,1).equalsIgnoreCase("a")||str.substring(0,1).equalsIgnoreCase("e")||
-                str.substring(0,1).equalsIgnoreCase("i")||str.substring(0,1).equalsIgnoreCase("o")||
-                str.substring(0,1).equalsIgnoreCase("u")){
+        System.out.println(str);
 
-            strNew += str.substring(1) + str.substring(0,1) + "ay";
+        if(vowels.indexOf(str.substring(0,1)) != -1){
+
+            strNew += str + "yay";
             return strNew;
 
-        }
-        else if (str.substring(0,1).equalsIgnoreCase("y")){
-            strNew += str.substring(1) + "yay";
-            return strNew;
         }
         else{
-            strNew += str.substring(1) + str.substring(0,1) + "yay";
-            return strNew;
+            if (vowels.indexOf(str.substring(1,2)) == -1) {
+                strNew += str.substring(2) + str.substring(0, 2) + "ay";
+                return strNew;
+            }
+            else{
+                strNew += str.substring(1) + str.substring(0, 1) + "ay";
+                return strNew;
+            }
         }
     }
 }
